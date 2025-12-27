bbebe9e3d618bbb896e02e3fccce6b045c87156c
vinayakstha <vsth005@gmail.com>
2025-12-27 16:40:26 +0545

    login form color changed

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "../_components/LoginForm";
 
 export default function Page() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+    <div className="min-h-screen flex items-center justify-center bg-white p-4">
       <LoginForm></LoginForm>
     </div>
   );
