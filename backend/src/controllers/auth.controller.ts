0eafd5b4239bd1e9fcae58ff9108400547907b30
vinayakstha <vsth005@gmail.com>
2026-01-05 00:03:09 +0545

    login function in auth controller

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -20,11 +20,35 @@ export class AuthController {
         .status(201)
         .json({ success: true, message: "User Created", data: newUser });
     } catch (error: Error | any) {
+      return res.status(error.statusCode ?? 500).json({
+        success: false,
+        message: error.message || "Internal server error",
+      });
+    }
+  }
+
+  async login(req: Request, res: Response) {
+    try {
+      const parsedData = LoginUserDTO.safeParse(req.body);
+      if (!parsedData.success) {
+        return res
+          .status(400)
+          .json({ success: false, message: z.prettifyError(parsedData.error) });
+      }
+      const loginData: LoginUserDTO = parsedData.data;
+      const { token, user } = await userService.loginUser(loginData);
+      return res.status(200).json({
+        success: true,
+        message: "Login successful",
+        data: user,
+        token,
+      });
+    } catch (error: Error | any) {
       return res
         .status(error.statusCode ?? 500)
         .json({
           success: false,
-          message: error.message || "Internal server error",
+          message: error.message || "Internal Server Error",
         });
     }
   }
