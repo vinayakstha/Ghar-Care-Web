bbc707bafc1320d2080472da742fc96b1bc4d0cd
vinayakstha <vsth005@gmail.com>
2026-01-04 11:02:34 +0545

    user dtos

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,1 +1,29 @@
 import mongoose, { Document, Schema } from "mongoose";
+import { UserType } from "../types/user.type";
+
+const UserSchema: Schema = new Schema<UserType>(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    phoneNumber: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export interface IUser extends UserType, Document {
+  _id: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export const UserModel = mongoose.model<IUser>("User", UserSchema);
