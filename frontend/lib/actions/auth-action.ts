ead2df93edcedaea046810013a97adaf59ec2661
vinayakstha <vsth005@gmail.com>
2026-01-17 23:34:33 +0545

    handelRegister action

--- frontend/lib/actions/auth-action.ts
+++ frontend/lib/actions/auth-action.ts
@@ -1,3 +1,26 @@
 "use server";
 
 import { registerUser, loginUser } from "../api/auth";
+import { setUserData, setAuthToken } from "../cookie";
+
+export const handleRegister = async (formData: any) => {
+  try {
+    const result = await registerUser(formData);
+    if (result.success) {
+      return {
+        success: true,
+        message: "registration successful",
+        data: result.data,
+      };
+    }
+    return {
+      success: false,
+      message: result.message || "Registration failed",
+    };
+  } catch (err: Error | any) {
+    return {
+      success: false,
+      message: err.message || "Registration failed",
+    };
+  }
+};
