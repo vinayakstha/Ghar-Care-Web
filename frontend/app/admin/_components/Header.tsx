8be0c39bb443f6ef70b51bae5012733330a22dec
vinayakstha <vsth005@gmail.com>
2026-02-10 14:59:04 +0545

    logout model added

--- frontend/app/admin/_components/Header.tsx
+++ frontend/app/admin/_components/Header.tsx
@@ -1,34 +1,52 @@
 "use client";
 
-import React from "react";
+import { useState } from "react";
 import { LogOut } from "lucide-react";
 import { useAuth } from "@/context/AuthContext";
+import { toast } from "react-toastify";
+import LogoutModal from "@/app/_components/LogoutModal";
 
 export default function Header() {
-  const handleLogout = () => {
-    // your logout logic here
-    console.log("Logged out!");
-  };
   const { logout } = useAuth();
 
-  return (
-    <header className="flex justify-between items-center px-4 py-3 bg-white shadow">
-      <div className="flex items-center gap-3">
-        <img
-          src="/images/logo.png"
-          alt="Logo"
-          className="w-8 h-8 object-contain rounded-full"
-        />
-        <h1 className="text-lg font-semibold text-gray-800">Admin</h1>
-      </div>
+  const [isLogoutOpen, setIsLogoutOpen] = useState(false);
 
-      {/* Right: Logout button */}
-      <button
-        onClick={logout}
-        className="px-4 py-1 text-red-500 font-medium text-sm cursor-pointer"
-      >
-        <LogOut size={17} />
-      </button>
-    </header>
+  // CONFIRM LOGOUT
+  const handleConfirmLogout = () => {
+    logout();
+    setIsLogoutOpen(false);
+    toast.success("Logout successful");
+  };
+
+  return (
+    <>
+      <header className="flex justify-between items-center px-4 py-3 bg-white shadow">
+        <div className="flex items-center gap-3">
+          <img
+            src="/images/logo.png"
+            alt="Logo"
+            className="w-8 h-8 object-contain rounded-full"
+          />
+          <h1 className="text-lg font-semibold text-gray-800">Admin</h1>
+        </div>
+
+        {/* Logout button */}
+        <button
+          onClick={() => setIsLogoutOpen(true)}
+          className="px-4 py-1 text-red-500 hover:text-red-600 transition cursor-pointer"
+        >
+          <LogOut size={17} />
+        </button>
+      </header>
+
+      {/* LOGOUT MODAL */}
+      <LogoutModal
+        isOpen={isLogoutOpen}
+        onClose={() => setIsLogoutOpen(false)}
+        onConfirm={handleConfirmLogout}
+        title="Logout"
+        description="Are you sure you want to log out of your account?"
+      />
+    </>
   );
 }
